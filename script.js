e3fc62eba7aa7da11c53475c7a24854ff5c122c6
ArturRL <artur.rod.lopes@gmail.com>
2022-07-12 20:01:09 -0300

    Desafio Entregue

---
+++ script.js
@@ -0,0 +1,30 @@
+let nome = "blablabla"
+
+let idade = 18
+
+let human = true
+
+let array = ["Artur", "Danda", 18, false]
+
+let cadastro = {
+    nome: "Artur",
+    idade: 18,
+    amigos: ["Jão", "Mari","Danda"]
+}
+
+// 
+
+let nome1 = "joy"
+console.log(nome1)
+let number = 20
+let ano = 2022
+let resultado = ano - number
+console.log(resultado)
+//
+let soma = ano + number
+console.log(soma)
+
+console.log("blablabla")
+
+let quartaFeira = false
+console.log(typeof quartaFeira)
